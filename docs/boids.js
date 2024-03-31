d4680536e5ce611ace4bf00e9a2ed457bdda0343
Kenan Alizadeh <kananalizade@gmail.com>
2024-03-31 21:56:51 +0400

    docs

--- docs/boids.js
+++ docs/boids.js
@@ -2143,7 +2143,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('docs/boids.wasm', import.meta.url);
+        input = new URL('boids.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
